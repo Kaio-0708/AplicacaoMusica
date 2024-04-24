85a991a31fdcb3de0db958605f0f18efaa1080ce
Kaio-0708 <kaiovitor0707@gmail.com>
2024-04-24 16:40:49 -0300

    teste

--- src/br/com/alura/minhasmusicas/principal/Principal.java
+++ src/br/com/alura/minhasmusicas/principal/Principal.java
@@ -1,2 +1,39 @@
-package br.com.alura.minhasmusicas.principal;public class Principal {
+package br.com.alura.minhasmusicas.principal;
+
+import br.com.alura.minhasmusicas.modelos.MinhasPreferidas;
+import br.com.alura.minhasmusicas.modelos.Musica;
+import br.com.alura.minhasmusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++){
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+           minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setApresentador("Kaio Vitor");
+        meuPodcast.setDescricao("Podcast sobre a importância do Java");
+        meuPodcast.setTitulo("Bolha Dev");
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+
+    }
 }
